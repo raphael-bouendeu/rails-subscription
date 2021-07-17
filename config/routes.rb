0bb63f66f02eae5323c665f83786de6cb08328d2
bouendeu raphael <bouendeu_raphael@yahoo.fr>
2021-07-17 09:15:40 +0200

    static controller with pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+ root 'static_public#landing_page'
+ get 'privacy',to: 'static_public#privacy'
+ get 'terms',to: 'static_public#terms'
   resources :posts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
