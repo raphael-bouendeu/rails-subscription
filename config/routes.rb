312be984e2238a7e8c301abee0e0cf4d800f3dbd
bouendeu raphael <bouendeu_raphael@yahoo.fr>
2021-07-18 10:13:52 +0200

    added user template and count

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
  get 'privacy',to: 'static_public#privacy'
  get 'terms',to: 'static_public#terms'
   resources :posts
-
+  resources :users,only: [:index,:show]
 end
